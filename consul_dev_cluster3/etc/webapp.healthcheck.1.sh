059342c90a1462f7c6c2be0994678855b4632d69
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-07-18 10:57:00 +0200

    refactor

--- consul_dev_cluster3/etc/webapp.healthcheck.1.sh
+++ consul_dev_cluster3/etc/webapp.healthcheck.1.sh
@@ -2,24 +2,28 @@
 
 SERVICE_URL="http://localhost:8080"
 
-which lynx &> /dev/null && {
-		
-		STATE_1=`lynx --dump $SERVICE_URL`
-		STATE_2=`lynx --dump $SERVICE_URL`
-		
-		if [ "$STATE_2" -gt "$STATE_1" ]; then
-		
-			echo "Service OK"
-			exit 0
-			
-		else
-			echo "Service KO"
-			exit 2
-		fi
+which lynx &> /dev/null || {
+	apt-get update
+	apt-get install -y lynx
+	apt-get clean
 }
+	
+#set initial values
+STATE_1="notanumber1"
+STATE_2="notanumber2"
 
-echo "No lynx command found, no way to check"
+#get some output of the service
+STATE_1=`lynx --dump $SERVICE_URL`
+STATE_2=`lynx --dump $SERVICE_URL`
 
-exit 1
-
-
+#the test
+if [ "$STATE_1" -eq 0 ] || [ "$STATE_2" -eq 0 ] ; then
+	echo "Service KO"
+	exit 2
+elif [ "$STATE_2" -gt "$STATE_1" ]; then
+	echo "Service OK"
+	exit 0
+else
+	echo "Service KO"
+	exit 2
+fi
