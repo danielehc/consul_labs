440f84c42cb5420a0db654bcbaf80978d749adf0
danielehc <daniele@hashicorp.com>
2018-07-20 11:22:45 +0200

    Check travis environment variable

--- build_scripts/travis_build.sh
+++ build_scripts/travis_build.sh
@@ -2,4 +2,19 @@
 
 echo "Running the build script"
 
+COMMIT=${TRAVIS_COMMIT_MESSAGE}
+
+if [[ $COMMIT ]]; then
+
+	echo "Commit message is: ${COMMIT}"
+
+else
+
+	echo "Commit message isnot set"
+
+fi
+
+#~ echo "Finding build folder"
+
+
 exit 0
