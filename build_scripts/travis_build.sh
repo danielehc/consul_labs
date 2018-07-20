aab8afec81288f857de407548d56c9eb2562d88c
danielehc <daniele@hashicorp.com>
2018-07-20 14:38:39 +0200

    Build script updated

--- build_scripts/travis_build.sh
+++ build_scripts/travis_build.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+set -x
+
 echo "Running the build script"
 
 if [[ ! $BUILD_DIR ]]; then
 		SRC_DIR=consul_dev_cluster4
 fi
 
-go get ./...
-go build ${SRC_DIR}/src/modern_app_web.go
+# go get ./...
+go build ${SRC_DIR}/src/modern_app_web.go -O modern_app_web
 
 find . -name modern_app_web
+
+set +x
 
 exit 0
