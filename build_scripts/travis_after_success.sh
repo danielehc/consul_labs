7db0aad413459f0bf45aff73b8084159ec0bc885
danielehc <daniele@hashicorp.com>
2018-07-23 15:07:46 +0200

    After success Docker

--- build_scripts/travis_after_success.sh
+++ build_scripts/travis_after_success.sh
@@ -10,9 +10,9 @@ fi
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO="danielehc/consul_labs"
 
-docker build -f Dockerfile -t ${REPO} .
-docker tag ${REPO} ${REPO}:${TRAVIS_TAG}
-docker tag ${REPO} ${REPO}:consul_labs-${TRAVIS_TAG}
+docker build -f Dockerfile -t ${REPO}:${TRAVIS_TAG} .
+#~ docker tag ${REPO} ${REPO}:${TRAVIS_TAG}
+#~ docker tag ${REPO} ${REPO}:consul_labs-${TRAVIS_TAG}
 docker push ${REPO}
 
 
